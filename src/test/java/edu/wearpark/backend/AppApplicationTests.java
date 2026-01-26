4ee9cde2d182d933f52acf49dc75be155c96a776
Marek Gromko <marek.gromko@hotmail.ca>
2026-01-26 13:08:26 -0500

    init commit

---
+++ src/test/java/edu/wearpark/backend/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.wearpark.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
