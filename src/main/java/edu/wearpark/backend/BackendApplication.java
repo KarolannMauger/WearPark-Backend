952cafdfe62553a7894b44dba69b4c3cc903467f
Marek Gromko <marek.gromko@hotmail.ca>
2026-02-07 08:26:00 -0500

    build: fix deps and some init issues

--- src/main/java/edu/wearpark/backend/BackendApplication.java
+++ src/main/java/edu/wearpark/backend/BackendApplication.java
@@ -1,9 +1,13 @@
 package edu.wearpark.backend;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.mongodb.autoconfigure.MongoAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+		MongoAutoConfiguration.class
+})
 public class BackendApplication {
 
 	public static void main(String[] args) {
