a462aed2ea099ea2e7c86c6d98cafc454e8ec82d
Marek Gromko <marek.gromko@hotmail.ca>
2026-02-07 10:41:14 -0500

    feat: motionEntry controller

---
+++ src/main/java/edu/wearpark/backend/repository/MotionEntryRepository.java
@@ -0,0 +1,30 @@
+package edu.wearpark.backend.repository;
+
+import edu.wearpark.backend.domain.MotionEntry;
+import org.bson.types.ObjectId;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+
+import java.time.Instant;
+import java.util.List;
+import java.util.Optional;
+
+public interface MotionEntryRepository extends MongoRepository<MotionEntry, ObjectId> {
+    @Query(
+            value = """
+                {
+                    'user_id': ?0,
+                    'start': { $gte: ?1, $lte: ?2 }
+                }
+                """,
+            sort = "{ 'start': -1 }"
+    )
+    List<MotionEntry> findBetween(ObjectId userId, Instant start, Instant end);
+
+    @Query(
+            value = "{ 'user_id': ?0 }",
+            sort  = "{ 'start': -1 }"
+    )
+    Optional<MotionEntry> findLatest(ObjectId userId);
+}
