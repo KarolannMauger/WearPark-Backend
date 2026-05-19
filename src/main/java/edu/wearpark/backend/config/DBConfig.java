63b7c602883f2bbaaed0ca6c36989a03fac62327
LoulouPlou <leia.plourde@hotmail.com>
2026-05-19 12:55:39 -0400

    fix: admin access

--- src/main/java/edu/wearpark/backend/config/DBConfig.java
+++ src/main/java/edu/wearpark/backend/config/DBConfig.java
@@ -3,15 +3,20 @@ package edu.wearpark.backend.config;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.persistence.autoconfigure.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.MongoDatabaseFactory;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
+import org.springframework.data.mongodb.core.convert.DbRefResolver;
+import org.springframework.data.mongodb.core.convert.DefaultDbRefResolver;
+import org.springframework.data.mongodb.core.convert.MappingMongoConverter;
+import org.springframework.data.mongodb.core.convert.MongoCustomConversions;
+import org.springframework.data.mongodb.core.mapping.MongoMappingContext;
+
+import java.util.Collections;
 
 @Configuration
 @EnableMongoAuditing
-@EntityScan("edu.wearpark.backend")
 public class DBConfig {
     @Value("${db.uri}")
     protected String DB_URI;
@@ -27,5 +32,16 @@ public class DBConfig {
             MongoClient mongoClient
     ) {
         return MongoDatabaseFactory.create(mongoClient, DB_NAME);
+    }
+
+    @Bean
+    public MappingMongoConverter mappingMongoConverter(
+            MongoDatabaseFactory factory,
+            MongoMappingContext context
+    ) {
+        DbRefResolver dbRefResolver = new DefaultDbRefResolver(factory);
+        MappingMongoConverter converter = new MappingMongoConverter(dbRefResolver, context);
+        converter.setCustomConversions(new MongoCustomConversions(Collections.emptyList()));
+        return converter;
     }
 }
