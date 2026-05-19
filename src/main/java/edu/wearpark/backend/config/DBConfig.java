edf1d9573a9c71e741bb55ee449efc470dd2a297
LoulouPlou <leia.plourde@hotmail.com>
2026-05-19 14:35:37 -0400

    fix: debug

--- src/main/java/edu/wearpark/backend/config/DBConfig.java
+++ src/main/java/edu/wearpark/backend/config/DBConfig.java
@@ -5,8 +5,16 @@ import com.mongodb.client.MongoClients;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.data.convert.ReadingConverter;
+import org.springframework.data.convert.WritingConverter;
 import org.springframework.data.mongodb.MongoDatabaseFactory;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
+import org.springframework.data.mongodb.core.convert.MongoCustomConversions;
+
+import java.time.Instant;
+import java.util.Date;
+import java.util.List;
 
 @Configuration
 @EnableMongoAuditing
@@ -25,5 +33,27 @@ public class DBConfig {
             MongoClient mongoClient
     ) {
         return MongoDatabaseFactory.create(mongoClient, DB_NAME);
+    }
+
+    @Bean
+    public MongoCustomConversions mongoCustomConversions() {
+        return new MongoCustomConversions(List.of(
+                new DateToInstantConverter(),
+                new InstantToDateConverter()
+        ));
+    }
+
+    @WritingConverter
+    static class InstantToDateConverter implements Converter<Instant, Date> {
+        public Date convert(Instant source) {
+            return Date.from(source);
+        }
+    }
+
+    @ReadingConverter
+    static class DateToInstantConverter implements Converter<Date, Instant> {
+        public Instant convert(Date source) {
+            return source.toInstant();
+        }
     }
 }
