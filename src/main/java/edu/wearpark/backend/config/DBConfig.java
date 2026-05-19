cedb4ef531fa71b61a00f0d491faf6e9ea2758bb
LoulouPlou <leia.plourde@hotmail.com>
2026-05-19 15:25:38 -0400

    fix: last debug test

--- src/main/java/edu/wearpark/backend/config/DBConfig.java
+++ src/main/java/edu/wearpark/backend/config/DBConfig.java
@@ -6,10 +6,12 @@ import com.mongodb.lang.NonNull;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.data.convert.ReadingConverter;
 import org.springframework.data.convert.WritingConverter;
 import org.springframework.data.mongodb.MongoDatabaseFactory;
+import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 import org.springframework.data.mongodb.core.convert.MongoCustomConversions;
 
@@ -19,7 +21,7 @@ import java.util.List;
 
 @Configuration
 @EnableMongoAuditing
-public class DBConfig {
+public class DBConfig extends AbstractMongoClientConfiguration {
     @Value("${db.uri}")
     protected String DB_URI;
     @Value("${db.name}")
@@ -29,15 +31,22 @@ public class DBConfig {
     public MongoClient mongoClient() {
         return MongoClients.create(DB_URI);
     }
+
     @Bean
+    @Primary
     public MongoDatabaseFactory mongoDatabase(
             MongoClient mongoClient
     ) {
         return MongoDatabaseFactory.create(mongoClient, DB_NAME);
     }
 
-    @Bean
-    public MongoCustomConversions mongoCustomConversions() {
+    @Override
+    protected String getDatabaseName() {
+        return DB_NAME;
+    }
+
+    @Override
+    public MongoCustomConversions customConversions() {
         return new MongoCustomConversions(List.of(
                 new DateToInstantConverter(),
                 new InstantToDateConverter()
@@ -54,7 +63,8 @@ public class DBConfig {
 
     @ReadingConverter
     static class DateToInstantConverter implements Converter<Date, Instant> {
-        public Instant convert(Date source) {
+        @Override
+        public Instant convert(@NonNull Date source) {
             return source.toInstant();
         }
     }
